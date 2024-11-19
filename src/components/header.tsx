14e16dc510e915d8b60392a293c891cb8f2d1140
9days <dev9days@gmail.com>
2024-11-19 17:07:28 +0900

    github  아이콘 추가

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Github from '@/constant/icon/github';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className="flex justify-between border-b border-gray-400 py-4">
-      <div className="">hamburger</div>
+      <div className="">9days.dev</div>
       <div className="flex">
         <div className="">theme,</div>
-        <div className="">github</div>
+        <Link href="https://github.com/Dev9Days/9days.dev" target="_blank">
+          <Github />
+        </Link>
       </div>
     </div>
   );
